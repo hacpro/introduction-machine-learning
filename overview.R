c81c1e437bb262a95c82ce9790d3da19e628e2e3
huli <huli@eitschei.com>
2017-07-04 00:38:11 +0200

    Data science questions added.

--- overview.R
+++ overview.R
@@ -6,6 +6,10 @@ df <- read.csv2("C:\\Users\\ch0125\\Dropbox\\Machine Learning\\part_1_data.csv",
 summary(df)
 
 # Datensatz ohne Differenzierung (Können wir einen Preis vorhersagen)
+# - How much/many?
+
+ggplot(df, aes(price)) +
+  geom_histogram()
 
 # Regression (Welche Variablen könnten wir einander gegenüberstellen?)
 ggplot(df, aes(price, sqft, col = df$in_sf)) +
@@ -17,9 +21,13 @@ summary(lm(price ~ sqft, df))
 # Mehrere Variablen kombinieren
 summary(lm(price ~ sqft+elevation+in_sf, df))
 
+# predict()
+
 # Datensatz mit Differenzierung (Können wir Muster zwischen SF und NY erkennen)
 
 # Korrelationsmatrix (Kann jemand ein Muster erkennen mit dem wir weiterarbeiten?)
+# - Is this A or B?
+
 df2 <- df
 df2$in_sf <- as.factor(df[, 1])
 ggpairs(data=df2, columns = c(4,5,6,7,8), title="Korrelationsmatrix",
