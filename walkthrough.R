5fc65927351cadf9865f4e20584be92c349d055a
huli <huli@eitschei.com>
2017-10-19 15:15:53 +0200

    Filed deleted which are no longer nescessary.

--- walkthrough.R
+++ walkthrough.R
@@ -486,7 +486,7 @@ result = AnomalyDetectionTs(raw_data,
 result$plot
 
 # --------------------------------------------------------------------------
-# Frage: Was soll ich als nächstes tun
+# Frage: Was soll ich als nächstes tun?
 # Methode: Reinforcment Learning
 # --------------------------------------------------------------------------
 
@@ -535,9 +535,66 @@ policy(model)["B..BX...X"]
 # (see environment function)
 
 # --------------------------------------------------------------------------
+# Frage: Was soll ich als naechstes tun?
+# Methode: Recommenders
+# --------------------------------------------------------------------------
 
 
+#install.packages("recommenderlab")
+#install.packages("data.table")
+#install.packages("dplyr")
 
-# Weiterführende Themen
-# Ensemble Learning: Randon Forest
-# Deep Learning: Neural Networks mnist
+library(recommenderlab)
+library(data.table)
+library(dplyr)
+
+ratings <- read.csv("movielens-datasets/ratings.csv")
+movies_and_genres <- read.csv("movielens-datasets/movies_and_genres.csv",
+                              stringsAsFactors=FALSE) 
+
+
+# Aus ratings eine Matrix machen 
+# (Rows = userId, Columns = movieId)
+ratingmat <- dcast(ratings, userId~movieId, 
+                   value.var = "rating", 
+                   na.rm=FALSE)
+ratingmat <- as.matrix(ratingmat[,-1]) # UserId entfernen
+
+# Die Matrix in eine 'sparse' recommanderlab Matrix verwandeln
+ratingmat <- as(ratingmat, "realRatingMatrix")
+
+# Normalisieren der Daten
+ratingmat_norm <- normalize(ratingmat)
+
+# UBFC Recommender Model erzeugen 
+# (UBCF stands for User-Based Collaborative Filtering)
+# Similarity Calculation Method: Cosine Similarity
+recommender_model <- Recommender(ratingmat_norm, 
+                                 method = "UBCF", 
+                                 param=list(method="Cosine",
+                                            nn=30))
+
+# Die top 10 Empfehlungen fuer Benutzer 1 in ratingset bestimmen
+recommendations <- predict(recommender_model, 
+                           ratingmat[1], n=10) 
+
+# Recommenderelab objekt in lesbare Liste verwandeln
+recommendations_list <- unlist(as(recommendations, "list"))
+
+# Funktion zum bestimmen von Titeln von Movie-Id
+getMovieTitle <- function(id)
+{
+  movies_and_genres[movies_and_genres$movieId == id, ]$title
+}
+
+# Was das System dem Benutzer 1 vorschlagen wuerde
+lapply(recommendations_list, getMovieTitle) %>% unlist
+
+# Welche Filme der User bereits mit einer Bewertung >= 3.5 bewertet hat
+lapply(ratings[ratings$userId == 1 & ratings$rating >= 3.5, ]$movieId,
+       getMovieTitle) %>% unlist
+
+
+# Done, congrats!
+
+
