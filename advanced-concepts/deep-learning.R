e75fda7c96542e4ed478b55dc4d1e0fd6afe6c73
huli <huli@eitschei.com>
2017-10-23 21:31:36 +0200

    Proof of concept.

--- advanced-concepts/deep-learning.R
+++ advanced-concepts/deep-learning.R
@@ -1,23 +1,15 @@
 
-# https://github.com/h2oai/h2o-tutorials/tree/master/tutorials/deeplearning
+# --------------------------------------------------------------------------------
+# Methode: Neuronales Netz
+# Paket: h2o (https://www.h2o.ai/download/)
+# Input: Gelabeltes Dataset mit handgeschriebenen Zeichen
+# --------------------------------------------------------------------------------
 
-# https://www.r-bloggers.com/a-little-h2o-deeplearning-experiment-on-the-mnist-data-set/
+#install.packages("RCurl")
+#install.packages("jsonlite")
+#install.packages("h2o", type="source", 
+#                  repos="https://h2o-release.s3.amazonaws.com/h2o/rel-weierstrass/6/R")
 
-
-# Split datasets
-# http://h2o-release.s3.amazonaws.com/h2o/master/3552/docs-website/h2o-docs/datamunge/splitdatasets.html
-
-# https://kuanhoong.wordpress.com/2016/02/01/r-and-deep-learning-cnn-for-handwritten-digits-recognition/
-
-# as factors!
-# convert digit labels to factor for classification
-#training[,1]<-as.factor(training[,1])
-
-
-install.packages("RCurl")
-install.packages("jsonlite")
-install.packages("h2o", type="source", 
-                  repos="https://h2o-release.s3.amazonaws.com/h2o/rel-weierstrass/6/R")
 
 
 setwd("c:/source/introduction-machine-learning/advanced-concepts")
@@ -25,8 +17,6 @@ setwd("c:/source/introduction-machine-learning/advanced-concepts")
 library(h2o)
 library(ggplot2)
 
-# starts a local h2o server, alternativ waere amazon ec2 oder microsoft azure
-h2o.init(nthreads=-1, max_mem_size="4G")
 
 # Pfad
 mnist = read.csv( "data/mnist.csv" )
@@ -39,13 +29,18 @@ for(i in 1:100){
 }
 
 
+
+
 # faktoren
-mnist[,1]<-as.factor(mnist[,1])
+mnist[,1] <- as.factor(mnist[,1])
 
 train_rows <- sample(nrow(mnist), .7 * (nrow(mnist)))
 train <- mnist[train_rows,]
 test <- mnist[-train_rows,]
 
+
+# starts a local h2o server, alternativ waere amazon ec2 oder microsoft azure
+h2o.init(nthreads=-1, max_mem_size="4G")
 
 train_h2o <- as.h2o(train)
 test_h2o <- as.h2o(test)
@@ -57,7 +52,7 @@ nn_model = h2o.deeplearning(
   x = 2:785,
   training_frame = train_h2o,
   hidden = c(400, 200, 2, 200, 400 ),
-  epochs = 10, #epochs = 600,
+  epochs = 1, #epochs = 600,
   activation = "Tanh",
   autoencoder = TRUE
 )
@@ -75,22 +70,36 @@ qplot(DF.L3.C1,
       main = "Neural network: 400 - 200 - 2 - 200 - 400 ")
 
 
+
+nn_model = h2o.deeplearning(
+  x = 2:785,
+  y = 1,
+  training_frame = train_h2o,
+  hidden = c(400, 200, 2, 200, 400 ),
+  epochs = 1, #epochs = 600,
+  activation = "Tanh"
+)
+
+
 # Prediction for test set
 pred <- h2o.predict(nn_model, 
                           newdata = test_h2o[,-1])
 
-as.data.frame(pred)
+# Performance von unserem NN ausgeben
+# (Schau dir die Confusion-Matrix an)
+h2o.performance(nn_model, test_h2o)
 
 
+# Schauen wir uns die ersten 25 Ergebnisse noch visuell an
+par( mfrow = c(5,5), mai = c(0,0,0,0))
+for(i in 1:25){
+  y = as.matrix(test[i, 2:785])
+  dim(y) = c(28, 28)
+  image( y[,nrow(y):1], axes = FALSE, col = gray(255:0 / 255))
+  text(0.2, 0, pred$predict[i, 1], cex = 3, col = 2, pos = c(3,4))
+}
 
-summary(prediction$p1, exact_quantiles=TRUE)
-
-
-summary(prediction)
-perf <- h2o.performance(nn_model, test_h2o)
-
-
-# shut down virtual H2O cluster
+# Und wir beenden unser virtuelles H2O cluster wieder
 h2o.shutdown(prompt = FALSE)
 
 
